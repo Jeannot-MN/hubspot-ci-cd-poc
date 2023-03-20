2d4f3773e5ffcf966e0b1d8300c10b1cb7bb4800
JeannotMunganga <jeannotngalula@gmail.com>
2023-03-20 15:24:37 +0200

    Added script to programatically build every single react module.

--- build-react-modules.js
+++ build-react-modules.js
@@ -2,16 +2,18 @@ const { join, resolve } = require("path");
 const { readdir } = require("fs");
 const { execSync } = require("child_process");
 
-const react_modules_folder = join(__dirname, 'src', 'react');
+const react_modules_folder = join('./src', 'react');
 
 console.log(react_modules_folder);
 
-readdir('./src/react', { withFileTypes: true })
-    .filter(dir_content => dir_content.isDirectory())
-    .forEach(sub_dir => {
-        try {
-            execSync(`cd ./src/react/${sub_dir.name}`)
-            execSync('npm run build');
-            execSync('ls -l');
-        } catch {}
-    })
+readdir(react_modules_folder, { withFileTypes: true }, (err, files) => {
+    files.filter(file => file.isDirectory())
+        .forEach(sub_dir => {
+            try {
+                let sub_dir_path = join(react_modules_folder, sub_dir.name);
+                console.log('cd ' + sub_dir_path);
+                execSync(`cd ${sub_dir_path}`);
+                execSync(`pwd`);
+            } catch {}
+        })
+})
